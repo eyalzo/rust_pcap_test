3a0b5c7a4c22084cec89b07db95089c265843a26
eyalzo <eyalzo@gmail.com>
2022-08-04 03:00:54 +0300

    Moved packet processing to connections.rs

--- src/connections.rs
+++ src/connections.rs
@@ -1,7 +1,82 @@
+use std::collections::HashMap;
 use std::fmt;
 use std::net::Ipv4Addr;
 use std::time::Instant;
+use etherparse::{InternetSlice, SlicedPacket, TransportSlice};
+use pcap::Packet;
 
+#[derive(Debug, Clone)]
+pub struct Connections {
+    /// Active connection list
+    conn_list: HashMap<u128, Conn>,
+    /// All time counter of connections added to list, including removed ones
+    conn_count: u32,
+    /// All time packets count, including errors, duplicates, etc.
+    packet_count: u64,
+    /// Number of times the packet was not processed because capture was too short
+    packet_len_error_count: u64,
+}
+
+impl Connections {
+    pub fn new() -> Connections {
+        Connections {
+            conn_list: HashMap::new(),
+            conn_count: 0,
+            packet_count: 0,
+            packet_len_error_count: 0,
+        }
+    }
+
+    pub fn process_packet(&mut self, packet: &Packet) {
+        self.packet_count += 1;
+        if (packet.len() as u32) < packet.header.len {
+            self.packet_len_error_count += 1;
+            return;
+        }
+
+        // Parse
+        match SlicedPacket::from_ethernet(packet) {
+            Err(value) => println!("Err {:?}", value),
+            Ok(value) => {
+                // For TCP packets, there should be link, ip and transport values
+                if !value.ip.is_some() || !value.transport.is_some() { return; }
+
+                // IP addresses
+                match value.ip.unwrap() {
+                    InternetSlice::Ipv4(ip_header, _) => {
+                        match value.transport.unwrap() {
+                            TransportSlice::Tcp(tcp) => {
+                                // IP payload is already calculated, while TCP header is that 32-bit units (see RFC)
+                                let tcp_payload_len = ip_header.payload_len() - 4 * tcp.data_offset() as u16;
+                                let conn_sign = Conn::sign_by_tuple(ip_header.source_addr(),
+                                                                    tcp.source_port(),
+                                                                    ip_header.destination_addr(),
+                                                                    tcp.destination_port());
+                                let new_seq = self.conn_list.len() as u16 + 1;
+                                let conn = self.conn_list.entry(conn_sign).or_insert(Conn::new(new_seq));
+                                // A little odd way to count connections but it works for now
+                                if conn.packet_count == 0 { self.conn_count += 1; }
+                                conn.add_bytes(tcp_payload_len as u64);
+                                println!("      TCP {}: {:?}:{} => {:?}:{}, len {}, {:?}",
+                                         conn.sequence,
+                                         ip_header.source_addr(),
+                                         tcp.source_port(),
+                                         ip_header.destination_addr(),
+                                         tcp.destination_port(),
+                                         tcp_payload_len,
+                                         conn);
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
 pub struct Conn {
     /// When the structure was initialized
     start_time: Instant,
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod connections;
 
-use std::collections::HashMap;
-use etherparse::{InternetSlice, SlicedPacket, TransportSlice};
 use log::{debug, info, trace};
 use pcap::Device;
-use crate::connections::{Conn};
+use crate::connections::{Connections};
 
 fn main() {
     info!("Start pcap_test...");
 
     // Connection list
-    let mut conn_list: HashMap<u128, Conn> = HashMap::new();
+    // let mut conn_list: HashMap<u128, Conn> = HashMap::new();
 
     let device_list = Device::list().expect("Failed to get device list");
     info!("Device list has {} elements. Those with addresses:", device_list.len());
@@ -30,48 +28,10 @@ fn main() {
     info!("Default device: {:?} ({:?})", cap.get_datalink().get_name().unwrap(),
              cap.get_datalink().get_description().unwrap());
 
-    let mut packet_count = 0;
-    while let Ok(packet) = cap.next() {
-        packet_count += 1;
-        // println!("received packet! {:?}", packet);
-        trace!("   Packet {}, caplen: {}, packetlen: {}", packet_count, packet.len(), packet.header.len);
-        // Parse
-        match SlicedPacket::from_ethernet(&packet) {
-            Err(value) => println!("Err {:?}", value),
-            Ok(value) => {
-                // For TCP packets, there should be link, ip and transport values
-                if !value.ip.is_some() || !value.transport.is_some() { continue; }
+    let mut connections = Connections::new();
 
-                // IP addresses
-                match value.ip.unwrap() {
-                    InternetSlice::Ipv4(ip_header, _) => {
-                        match value.transport.unwrap() {
-                            TransportSlice::Tcp(tcp) => {
-                                // IP payload is already calculated, while TCP header is that 32-bit units (see RFC)
-                                let tcp_payload_len = ip_header.payload_len() - 4 * tcp.data_offset() as u16;
-                                let conn_sign = Conn::sign_by_tuple(ip_header.source_addr(),
-                                                                    tcp.source_port(),
-                                                                    ip_header.destination_addr(),
-                                                                    tcp.destination_port());
-                                let new_seq = conn_list.len() as u16 + 1;
-                                let conn = conn_list.entry(conn_sign).or_insert(Conn::new(new_seq));
-                                conn.add_bytes(tcp_payload_len as u64);
-                                println!("      TCP {}: {:?}:{} => {:?}:{}, len {}, {:?}",
-                                         conn.sequence,
-                                         ip_header.source_addr(),
-                                         tcp.source_port(),
-                                         ip_header.destination_addr(),
-                                         tcp.destination_port(),
-                                         tcp_payload_len,
-                                         conn);
-                            }
-                            _ => {}
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
+    while let Ok(packet) = cap.next() {
+        connections.process_packet(&packet);
     }
 
     println!("End pcap_test.");
