e14ee815cd0b75cdff950d7297b12f491ebd6e28
eyalzo <eyalzo@gmail.com>
2022-08-17 12:51:55 +0300

    CLI supports specifying the capture device.

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ A typical run with logs at DEBUG level (INFO is the default):
 ```bash
 RUSTFLAGS=-Awarnings RUST_LOG="debug" cargo run --release
 ```
+
+The CLI supports flags for device and filter for simpler development.
+For example, to capture only traffic to eyalzo.com (that supports clear text http), use this (the -d is optional):
+```bash
+RUSTFLAGS=-Awarnings RUST_LOG="trace" cargo run -- -f "host 50.87.176.106 and tcp" -d "en0"
+```
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ struct Cli {
     /// See http://biot.com/capstats/bpf.html for more information about this syntax.
     #[clap(short, long, value_parser, default_value = "tcp")]
     filter: String,
+    /// Device name to capture ("interface" in tcpdump terminology).
+    /// Defaults to the main device
+    #[clap(short, long, value_parser)]
+    device: Option<String>,
 }
 
 fn main() {
@@ -23,9 +27,24 @@ fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     info!("Start pcap_test...");
 
+    // Get the default device name, to be used later when looking at the device list
+    let mut main_device_name =
+        match Device::lookup() {
+            Err(error) => { panic!("Failed to get default pcap device: {}", error) }
+            Ok(device) => {
+                device.name
+            }
+        };
+
+    if args.device.is_some() {
+        main_device_name = String::from(args.device.unwrap());
+    }
+
+    let mut main_device: Option<Device> = None;
     let device_list = Device::list().expect("Failed to get device list");
     info!("Device list has {} elements. Those with addresses:", device_list.len());
     for cur_device in device_list {
+        if cur_device.name.eq(&main_device_name) { main_device = Some(cur_device.to_owned()); }
         if cur_device.addresses.len() <= 0 { continue; }
         debug!("   Device '{}' = {} ({} addresses)", cur_device.name,
                  cur_device.desc.unwrap_or(String::from("unknown")),
@@ -35,22 +54,14 @@ fn main() {
         }
     }
 
-    // Get the default device
-    let main_device =
-        match Device::lookup() {
-            Err(error) => { panic!("Failed to get default pcap device: {}", error) }
-            Ok(device) => {
-                info!("Default device: {:?}", device);
-                for cur_addr in &device.addresses {
-                    trace!("      {}", cur_addr.addr);
-                }
-                device
-            }
-        };
+    if main_device.is_none() {
+        panic!("Failed to find a (specified or default) device. \
+        Consider running with RUST_LOG=\"trace\" and watch the device list carefully.");
+    }
 
     let mut cap: Capture<Active> =
         {
-            match Capture::from_device(main_device).unwrap()
+            match Capture::from_device(main_device.unwrap()).unwrap()
                 .promisc(true)
                 .immediate_mode(true)
                 .snaplen(65535)
